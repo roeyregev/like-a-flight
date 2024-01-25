f46f0f5ff12e72c33fec8a374a959fcd07d76e88
Roey Regev <roeyregev@gmail.com>
2024-01-25 17:15:06 +0200

    react-select implemented in vacations list page

--- Frontend/src/Components/VacationsArea/FilterSelector/FilterSelector.tsx
+++ Frontend/src/Components/VacationsArea/FilterSelector/FilterSelector.tsx
@@ -1,59 +1,59 @@
-// import Select from 'react-select'
+import Select from 'react-select'
 import classNames from "classnames";
 import { Tabs } from "../VacationsList/VacationsList";
 import "./FilterSelector.css";
 import { SyntheticEvent } from "react";
+import filterIcon from "../../../Assets/Images/filter-icon.svg"
 
 type FiltersBarProps = {
     tabs: Tabs[]
     handleClickedTab: Function
 }
 
-
-
 function FilterSelector(props: FiltersBarProps): JSX.Element {
 
 
-    function handleChange(args: SyntheticEvent): void {
-        const selectElement: HTMLSelectElement = args.target as HTMLSelectElement;
-        const tabValue = +selectElement.value;
-        console.log("tabValue: " + tabValue);
-        props.handleClickedTab(tabValue);
+    // function handleChange(args: SyntheticEvent): void {
+    //     const selectElement: HTMLSelectElement = args.target as HTMLSelectElement;
+    //     const tabValue = +selectElement.value;
+    //     console.log("tabValue: " + tabValue);
+    //     props.handleClickedTab(tabValue);
+    // }
+
+
+    const tabs = [
+        { value: 1, label: "Show all flights" },
+        { value: 2, label: "Show my Liked flights" },
+        { value: 3, label: "Show current flights" },
+        { value: 4, label: "Show future flights" },
+    ]
+
+    function handleChange2(selectedOption: any): void {
+        props.handleClickedTab(selectedOption.value);
     }
-
-    // const options = [
-    //     { value: 'chocolate', label: 'Chocolate' },
-    //     { value: 'strawberry', label: 'Strawberry' },
-    //     { value: 'vanilla', label: 'Vanilla' }
-    // ]
-
-    // const tabs = [
-    //     { value: 1, label: "All" },
-    //     { value: 2, label: "My Likes" },
-    //     { value: 3, label: "Ongoing" },
-    //     { value: 4, label: "Future" },
-    // ]
-
-
-    // return (
-    //     <div className="FilterSelector">
-    //         <Select options={tabs} />
-    //         {/* <Select options={tabs} onChange={handleChange}/> */}
-    //     </div >
-    // );
 
     return (
         <div className="FilterSelector">
-            <div className="custom-select" >
-                <select name="filter-selector" onChange={handleChange}>
-                    {/* <select name="filter-selector" onChange = {() => props.handleClickedTab(t.id)}> */}
-                    {props.tabs.map(
-                        t => (<option defaultValue="1" className={classNames({ "active": t.isSelected })} value={t.id} key={t.id} >{t.name}</option>)
-                    )}
-                </select>
+            <div className="filter-flex">
+                <img src={filterIcon} alt="filter-icon" />
+                <Select options={tabs} onChange={handleChange2} defaultValue={tabs[0]} className="select-bar"/>
             </div>
         </div >
     );
+
+    // return (
+    //     <div className="FilterSelector">
+    //         <div className="custom-select" >
+    //             <select name="filter-selector" onChange={handleChange}>
+    //                 {/* <select name="filter-selector" onChange = {() => props.handleClickedTab(t.id)}> */}
+    //                 {props.tabs.map(
+    //                     t => (<option defaultValue="1" className={classNames({ "active": t.isSelected })} value={t.id} key={t.id} >{t.name}</option>)
+    //                 )}
+    //             </select>               
+    //         </div>
+    //         <Select options={tabs} />
+    //     </div >
+    // );
 }
 
 export default FilterSelector;

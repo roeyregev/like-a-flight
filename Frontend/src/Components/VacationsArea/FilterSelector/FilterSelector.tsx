0142d00e142b2e2e1b2ee06edb731254b72aa3b9
Roey Regev <roeyregev@gmail.com>
2024-01-26 16:19:26 +0200

    react-select fully styled

--- Frontend/src/Components/VacationsArea/FilterSelector/FilterSelector.tsx
+++ Frontend/src/Components/VacationsArea/FilterSelector/FilterSelector.tsx
@@ -1,9 +1,8 @@
-import Select from 'react-select'
-import classNames from "classnames";
+import Select, { CSSObjectWithLabel, StylesConfig } from 'react-select';
+import filterIcon from "../../../Assets/Images/filter-icon.svg";
 import { Tabs } from "../VacationsList/VacationsList";
 import "./FilterSelector.css";
-import { SyntheticEvent } from "react";
-import filterIcon from "../../../Assets/Images/filter-icon.svg"
+import { stat } from 'fs';
 
 type FiltersBarProps = {
     tabs: Tabs[]
@@ -12,21 +11,48 @@ type FiltersBarProps = {
 
 function FilterSelector(props: FiltersBarProps): JSX.Element {
 
-
-    // function handleChange(args: SyntheticEvent): void {
-    //     const selectElement: HTMLSelectElement = args.target as HTMLSelectElement;
-    //     const tabValue = +selectElement.value;
-    //     console.log("tabValue: " + tabValue);
-    //     props.handleClickedTab(tabValue);
-    // }
-
-
     const tabs = [
         { value: 1, label: "Show all flights" },
         { value: 2, label: "Show my Liked flights" },
         { value: 3, label: "Show current flights" },
         { value: 4, label: "Show future flights" },
     ]
+
+    const styles: StylesConfig<{ value: number; label: string }, false, any> = {
+        control: (provided: CSSObjectWithLabel, state: any) => ({
+            ...provided,
+            border: 'none',
+            boxShadow: 'none',
+            backgroundColor: '#FEEDD9',
+            color: '#9D3B3B',
+            padding: 0
+
+        }),
+        option: (provided: CSSObjectWithLabel, state: any) => ({
+            ...provided,
+            backgroundColor: state.isFocused ? '#FAE5CD' : '#FEEDD9',
+            color: state.isSelected ? '#9D3B3B' : '#9D3B3B',
+
+        }),
+        singleValue: (base: CSSObjectWithLabel, state: any) => ({
+            ...base,
+            color: '#9D3B3B',
+        }),
+        indicatorSeparator: (base: CSSObjectWithLabel, state: any) => ({
+            ...base,
+            backgroundColor: 'none',
+        }),
+        dropdownIndicator: (base: CSSObjectWithLabel, state: any) => ({
+            ...base,
+            color: '#FBAFAF', 
+            ':hover': {
+                color: '#EC8A8A', // Change this to the desired hover color
+            },
+            ':active': {
+                color: '#EC8A8A', // Change this to the desired active (clicked) color
+            },
+        }),
+    };
 
     function handleChange2(selectedOption: any): void {
         props.handleClickedTab(selectedOption.value);
@@ -36,24 +62,32 @@ function FilterSelector(props: FiltersBarProps): JSX.Element {
         <div className="FilterSelector">
             <div className="filter-flex">
                 <img src={filterIcon} alt="filter-icon" />
-                <Select options={tabs} onChange={handleChange2} defaultValue={tabs[0]} className="select-bar"/>
+                <Select options={tabs} onChange={handleChange2} defaultValue={tabs[0]} styles={styles} isSearchable={false}  className="select-bar" />
             </div>
         </div >
     );
-
-    // return (
-    //     <div className="FilterSelector">
-    //         <div className="custom-select" >
-    //             <select name="filter-selector" onChange={handleChange}>
-    //                 {/* <select name="filter-selector" onChange = {() => props.handleClickedTab(t.id)}> */}
-    //                 {props.tabs.map(
-    //                     t => (<option defaultValue="1" className={classNames({ "active": t.isSelected })} value={t.id} key={t.id} >{t.name}</option>)
-    //                 )}
-    //             </select>               
-    //         </div>
-    //         <Select options={tabs} />
-    //     </div >
-    // );
 }
 
 export default FilterSelector;
+
+
+// function handleChange(args: SyntheticEvent): void {
+//     const selectElement: HTMLSelectElement = args.target as HTMLSelectElement;
+//     const tabValue = +selectElement.value;
+//     console.log("tabValue: " + tabValue);
+//     props.handleClickedTab(tabValue);
+// }
+
+// return (
+//     <div className="FilterSelector">
+//         <div className="custom-select" >
+//             <select name="filter-selector" onChange={handleChange}>
+//                 {/* <select name="filter-selector" onChange = {() => props.handleClickedTab(t.id)}> */}
+//                 {props.tabs.map(
+//                     t => (<option defaultValue="1" className={classNames({ "active": t.isSelected })} value={t.id} key={t.id} >{t.name}</option>)
+//                 )}
+//             </select>
+//         </div>
+//         <Select options={tabs} />
+//     </div >
+// );

5a403502e25d2bcf623daf5a7d8e0ede5e48b42b
Roey Regev <roeyregev@gmail.com>
2024-02-03 23:36:15 +0200

    working home animation

--- Frontend/src/Components/HomeArea/Home/Home.tsx
+++ Frontend/src/Components/HomeArea/Home/Home.tsx
@@ -1,16 +1,18 @@
 import { useState } from "react";
 import { NavLink, useLocation, useNavigate } from "react-router-dom";
 import appConfig from "../../../Utils/AppConfig";
+import NotAdminPopup from "../../AuthArea/NotAdminPopup/NotAdminPopup";
 import NotLoggedInPopup from "../../AuthArea/NotLoggedInPopup/NotLoggedInPopup";
 import "./Home.css";
-import NotAdminPopup from "../../AuthArea/NotAdminPopup/NotAdminPopup";
+import homeAnimation from "../../../Assets/Animations/plane-aniamtion.json"
+import Lottie from "react-lottie";
 
 function Home(): JSX.Element {
     const navigate = useNavigate()
     const location = useLocation();
     const state = location.state;
     // const adminState = location.state;
-    
+
 
     const [showPopup, setShowPopup] = useState<boolean>(state?.showPopup);
     const [showAdminPopup, setShowAdminPopup] = useState<boolean>(state?.showAdminPopup);
@@ -28,6 +30,12 @@ function Home(): JSX.Element {
         console.log("close Admin popup")
     }
 
+    const homeAnimationData = {
+        animationData: homeAnimation,
+        loop: true,
+        autoplay: true,
+    }
+
     // { showPopup && (<NotLoggedInPopup closePopup={closePopup} />) }
     // console.log("showPopup" + showPopup + " " + location)
 
@@ -43,12 +51,12 @@ function Home(): JSX.Element {
                 <h2>For those who like to like</h2>
                 <p>Basically, we offer a very limited wishlist options.
                     It’s for vacations. yes that’s it. Our vacations list is also not so great. </p>
-                {/* <button onClick={handleClick}>Let’s like some flights!</button> */}
                 <NavLink to={appConfig.vacationsRoute}>Let’s like some flights!</NavLink>
             </div>
 
             <div className="home-illustration">
-
+                <Lottie options={homeAnimationData} />
+                <div className="clouds"></div>
             </div>
         </div>
     );

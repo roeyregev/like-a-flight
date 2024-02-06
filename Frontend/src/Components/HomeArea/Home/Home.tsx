14e71ef7347ca02d8748a7fd8166e5e8791cbf3f
Roey Regev <roeyregev@MacBook-Pro-3.local>
2024-02-06 14:29:51 +0200

    small css fixes

--- Frontend/src/Components/HomeArea/Home/Home.tsx
+++ Frontend/src/Components/HomeArea/Home/Home.tsx
@@ -1,30 +1,23 @@
 import { useState } from "react";
-import { NavLink, useLocation, useNavigate } from "react-router-dom";
+import Lottie from "react-lottie";
+import { NavLink, useLocation } from "react-router-dom";
+import homeAnimation from "../../../Assets/Animations/plane-aniamtion.json";
 import appConfig from "../../../Utils/AppConfig";
 import NotAdminPopup from "../../AuthArea/NotAdminPopup/NotAdminPopup";
 import NotLoggedInPopup from "../../AuthArea/NotLoggedInPopup/NotLoggedInPopup";
 import "./Home.css";
-import homeAnimation from "../../../Assets/Animations/plane-aniamtion.json"
-import Lottie from "react-lottie";
 
 function Home(): JSX.Element {
-    const navigate = useNavigate()
     const location = useLocation();
     const state = location.state;
-    // const adminState = location.state;
-
-
     const [showPopup, setShowPopup] = useState<boolean>(state?.showPopup);
     const [showAdminPopup, setShowAdminPopup] = useState<boolean>(state?.showAdminPopup);
-
-    // function handleClick() {
-    //     navigate("/vacations")
-    // }
 
     function closePopup() {
         setShowPopup(false);
         console.log("close popup")
     }
+
     function closeAdminPopup() {
         setShowAdminPopup(false);
         console.log("close Admin popup")
@@ -36,12 +29,6 @@ function Home(): JSX.Element {
         autoplay: true,
     }
 
-    // { showPopup && (<NotLoggedInPopup closePopup={closePopup} />) }
-    // console.log("showPopup" + showPopup + " " + location)
-
-    // { showAdminPopup && (<NotAdminPopup closePopup={closePopup} />) }
-    // console.log("showAdminPopup" + showPopup + " " + location)
-
     return (
         <div className="Home">
             {showPopup && (<NotLoggedInPopup closePopup={closePopup} />)}
@@ -51,12 +38,11 @@ function Home(): JSX.Element {
                 <h2>For those who like to like</h2>
                 <p>Basically, we offer a very limited wishlist options.
                     It’s for vacations. yes that’s it. Our vacations list is also not so great. </p>
-                <NavLink to={appConfig.vacationsRoute}>Let’s like some flights!</NavLink>
+                <NavLink className="main-btn" to={appConfig.vacationsRoute}>Let’s like some flights!</NavLink>
             </div>
 
             <div className="home-illustration">
                 <Lottie options={homeAnimationData} />
-                {/* <div className="clouds"></div> */}
             </div>
         </div>
     );
--- Frontend/src/Components/VacationsArea/PagesNavbar/PagesNavbar.tsx
+++ Frontend/src/Components/VacationsArea/PagesNavbar/PagesNavbar.tsx
@@ -1,5 +1,5 @@
+import arrowIcon from "../../../Assets/Images/arrow-icon-small.svg";
 import "./PagesNavbar.css";
-import arrowIcon from "../../../Assets/Images/arrow-icon-small.svg"
 
 type PagesNavbarProps = {
     pages: number
@@ -26,17 +26,11 @@ function PagesNavbar(props: PagesNavbarProps): JSX.Element {
         pagesArray.push(i);
     }
 
-    // function goToPage(pageNumber: number) {
-    //     console.log("Test")
-    // }
-
-
     return (
         <div className="PagesNavbar">
             <button onClick={previousPage} disabled={props.activePage <= 1}>
                 <img src={arrowIcon} alt="arrow-icon" />
             </button>
-            {/* {pagesArray.map(p => <button className="smallBtn" key={p} value={p} onClick={() => goToPage(p)}>{p}</button>)} */}
             <p>page {props.activePage}/{props.totalPages}</p>
             <button onClick={nextPage} disabled={props.activePage >= props.totalPages}>
                 <img src={arrowIcon} alt="arrow-icon" className="right" />
